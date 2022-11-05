500dc1060fd94f7214427954a4854feaeaec2a8e
Tim Göller <tim.goeller@posteo.de>
2022-11-06 00:37:13 +0100

    Add introduction

--- src/components/sidebar/WelcomeSidebarScreen.vue
+++ src/components/sidebar/WelcomeSidebarScreen.vue
@@ -3,18 +3,21 @@
   <div class="sidebar--section">
     <h2 class="sidebar--section-title">Introduction</h2>
     <div class="sidebar--section-content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+      <p>This is a tool which helps you to present you or your product in scenes all around the world! 🌍</p>
+
+      <p class="mt-1">This is how it works:</p>
+
+      <p class="mt-1">
+        <b>1. </b> Upload a mask image of choice and describe what it shows in the text box below. You can also skip this step.<br>
+        <b>2. </b> Click a point on the map at which you want to show your content.<br>
+        <b>3. </b> Modify the prompt to your likings.<br>
+        <b>4. </b> Click generate and enjoy! 🎉
+      </p>
+
+      <p class="mt-1">
+        ...or you can try our magic mode to get location suggestions!
+      </p>
     </div>
-  </div>
-  <div class="sidebar--section">
-    <h2 class="sidebar--section-title">Upload Image:</h2>
-    <div class="sidebar--section-content"><ImageUpload /></div>
   </div>
   <div class="sidebar--section">
     <h2 class="sidebar--section-title is-description">Image Description</h2>
@@ -27,6 +30,11 @@
       />
     </div>
   </div>
+  <div class="sidebar--section">
+    <h2 class="sidebar--section-title">Upload Image:</h2>
+    <div class="sidebar--section-content"><ImageUpload /></div>
+  </div>
+
 </template>
 
 <script setup>
